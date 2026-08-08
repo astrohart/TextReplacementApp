c9eee5171887956aa115bca36443b99ef851d74b
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-08-08 12:38:02 -0600

    Update XML docs for event handlers

--- TextReplacementApp/MainWindow.cs
+++ TextReplacementApp/MainWindow.cs
@@ -271,7 +271,7 @@ namespace TextReplacementApp
         /// event.
         /// </param>
         /// <param name="e">
-        /// A <see cref="T:System.EventArgs" /> that contains the event
+        /// An <see cref="T:System.EventArgs" /> that contains the event
         /// data.
         /// </param>
         /// <remarks>
